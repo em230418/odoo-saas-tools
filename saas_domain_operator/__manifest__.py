aff7711a84ed03b5d45778e79cff1bd8f572efc1
Eugene Molotov <molotov@it-projects.info>
2020-11-20 17:24:33 +0500

    [NEW] saas_domain_operator

---
+++ saas_domain_operator/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
+# License MIT (https://opensource.org/licenses/MIT).
+
+{
+    "name": """SaaS: domain operator""",
+    "category": "SaaS",
+    "images": [],
+    "version": "12.0.1.0.0",
+    "application": False,
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "apps@it-projects.info",
+    "website": "https://apps.odoo.com/apps/modules/13.0/saas_domain_operator/",
+    "license": "Other OSI approved licence",  # MIT
+    # "price": 9.00,
+    # "currency": "EUR",
+    "depends": [],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [],
+    "demo": [],
+    "qweb": [],
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+    "auto_install": False,
+    "installable": True,
+}
